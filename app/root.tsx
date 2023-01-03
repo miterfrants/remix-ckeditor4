4dc39861bc8ae2aa1e56bfb596de86d7f27e388f
Peter Huang <miterfrnats@gmail.com>
2023-01-03 20:50:39 +0800

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,48 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import { useEffect, useRef, useState } from "react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  const editorRef = useRef(null);
+  const { CKEditor } = editorRef.current || {};
+  useEffect(() => {
+    console.log(require("ckeditor4-react").CKEditor);
+    editorRef.current = {
+      CKEditor: require("ckeditor4-react").CKEditor, //Added .CKEditor
+    };
+    console.log("tsting");
+    setEditorLoaded(true);
+  }, []);
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const [data, setData] = useState("");
+
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {editorLoaded ? <CKEditor /> : <p>Carregando...</p>}
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
